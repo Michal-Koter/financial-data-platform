7276ef2738fafc45a27f9bca8186e806eaefa812
Koter Michal <s24569@pjwstk.edu.pl>
2025-05-15 00:54:01 +0200

    build: set up Eureka server

--- service-registry/src/main/java/com/finance/serviceregister/ServiceRegisterApplication.java
+++ service-registry/src/main/java/com/finance/serviceregister/ServiceRegisterApplication.java
@@ -2,8 +2,10 @@ package com.finance.serviceregister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegisterApplication {
 
     public static void main(String[] args) {
